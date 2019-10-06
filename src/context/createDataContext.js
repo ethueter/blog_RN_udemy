b6a878600f5a66fb0dea649953626d1cdfc5ad55
Tyler Hueter <tylerhueter08@gmail.com>
2019-10-05 22:52:28 -0400

    index screen styling, and delete button

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,12 +1,17 @@
 import React, { useReducer } from 'react';
 
-export default (reducer, action, initialState) => {
+export default (reducer, actions, initialState) => {
     const Context = React.createContext();
 
     const Provider = ({ children }) => {
         const [state, dispatch] = useReducer(reducer, initialState);
 
-        return <Context.Provider value={{state}}>
+        const boundActions = {};
+        for (let key in actions) {
+            boundActions[key] = actions[key](dispatch)
+        }
+
+        return <Context.Provider value={{state, ...boundActions}}>
             {children}
         </Context.Provider>
 
