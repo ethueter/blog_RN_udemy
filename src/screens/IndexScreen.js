6d6b427555676e6a5907b5bf93b3747444e175c6
Tyler Hueter <tylerhueter08@gmail.com>
2019-10-05 23:03:06 -0400

    added show screen with navigation

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
     const {state, addBlogPost, deleteBlogPost } = useContext(Context)
     return (
         <View>
@@ -12,13 +12,16 @@ const IndexScreen = () => {
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) => {
-                    return <View style={styles.row}>
-                        <Text style={styles.title}>{item.title} - {item.id}</Text>
-                        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                            <Feather name="trash" style={styles.icon} />
-                        </TouchableOpacity>
-                        
+                    return ( 
+                    <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather name="trash" style={styles.icon} />
+                            </TouchableOpacity>
                         </View>
+                    </TouchableOpacity>
+                    )
                 }}
             />
         </View>
